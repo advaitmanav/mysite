9802d3a70bc9f4050ac9fcf8ad5df786a76fcbe9
advaitmanav <72219487+advaitmanav@users.noreply.github.com>
2026-01-30 22:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const quotes = [
     { text: "The Beloved longs to see me, and I long even more to see Him.", author: "Ibn Arabi" },
     { text: "The Lover and the Beloved met, and the Beloved said to the Lover, 'Thou needest not to speak to me. Sing to me only with thine eyes, for they are words to my heart - that I may give thee that which thou dost ask.", author: "Ramon Llull" },
     { text: "Presence is the one we call the Beloved.", author: "Rumi" },
-    { text: "When uncreated light appears, there is nothing looking at it. It is present to itself. It is who we are.  We are not experiencing the state; we are the state.", author: "R.B" },
+    { text: "When uncreated light appears, there is nothing  looking at it. It is present to itself. It is who we are.  We are not experiencing the state; we are the state.", author: "R.B" },
     { text: "For that which sees is itself and the thing which is seen. ", author: "Plotinus" },
     { text: "You may try a hundred things, but only love can release you from your self. Unless your chest gets that burning feeling every time love is mentioned, you are not yet ready to conquer love.", author: "Rumi" },
     { text: "The sequence is the second best plan. Uncreated Light is the best plan.", author: "R.B." },
-    { text: "From love, bitter becomes sweet.From love, thorns become flowers.From love, vinegar becomes wine.From love, fire becomes light. From love, devil becomes angel. From love, sorrow becomes joy. From love, sickness becomes health. From love, fury becomes mercy. From love, dead becomes alive. From love, king becomes servant. Love knows a hundred ways of showing off. Love flirts with a lover all the time. Knowing how to flirt back with love, that’s how you get your hands on love! You may try a hundred things, but only love can release you from your self. Unless your chest gets that burning feeling every time love is mentioned, you are not yet ready to conquer love.", author: "Poem by Rumi" },
-
+    { text: "The sequence is the second best plan. Uncreated Light is the best plan.", author: "R.B." },
+    { text: "When you love something or someone will all your heart, there is no backup option.", author: "Advait 2024" },
+    { text: "When you give with expectations in return, you're already lost in the first place", author: "Advait 2025" },
+   
 ];
 
 const quoteEl = document.getElementById('quote');
